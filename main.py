cc20e85ef6488184ad4651f0e44aa46be0df40c0
oblivion4us <nft4oblivion@gmail.com>
2024-08-12 14:14:35 +0200

    all window settings

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 pygame.init()
@@ -7,9 +8,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("images/titleimg.jpg")
+pygame.display.set_icon(icon)
 
+target_img = pygame.image.load("images/targetimg.png")
+target_width = 50
+target_height = 50
 
+target_x = random.randint(0, SCREEN_WIDTH-target_width)
+target_y = random.randint(0, SCREEN_HEIGHT-target_height)
+
+color = (220,220,220)
 
 running = True
 while running:
